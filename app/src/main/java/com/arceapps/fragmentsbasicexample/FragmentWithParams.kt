978bee3c14db6d6d03e0190a947df967779d97f2
ArceApps <arce.com.apps@gmail.com>
2022-12-05 21:06:25 +0100

    version 1.0

---
+++ app/src/main/java/com/arceapps/fragmentsbasicexample/FragmentWithParams.kt
@@ -0,0 +1,37 @@
+package com.arceapps.fragmentsbasicexample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FragmentWithParams : Fragment() {
+    companion object {
+        const val ARG_PARAM1 = "param1"
+        const val ARG_PARAM2 = "param2"
+    }
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+            Log.d("params: ", "$param1 $param2")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_with_params, container, false)
+    }
+}
--- app/src/main/java/com/arceapps/fragmentsbasicexample/MainActivity.kt
+++ app/src/main/java/com/arceapps/fragmentsbasicexample/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.arceapps.fragmentsbasicexample
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null) {
+            addSecondFragment()
+            addFragmentWithParams()
+        }
+    }
+
+    private fun addSecondFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<SecondFragment>(R.id.fragment_container2)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun addFragmentWithParams() {
+        val bundle: Bundle = bundleOf(FragmentWithParams.ARG_PARAM1 to "FragmentParam1",
+        FragmentWithParams.ARG_PARAM2 to "FragmentParam2")
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<FragmentWithParams>(R.id.fragment_container3, args = bundle)
+        }
+        val textView = findViewById<TextView>(R.id.textView)
+        val parameter1 = bundle.getString("param1").orEmpty()
+        val parameter2 = bundle.getString("param2").orEmpty()
+        textView.text = "$parameter1 - $parameter2"
     }
 }
